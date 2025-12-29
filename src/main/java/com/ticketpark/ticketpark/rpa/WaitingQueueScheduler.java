3e236b10bac3f3d819f00b3f493c9c8af1c7628f
songsh927 <songsh96927@gmail.com>
2025-12-30 00:45:08 +0900

    디큐 초당 80건 증가

--- src/main/java/com/ticketpark/ticketpark/rpa/WaitingQueueScheduler.java
+++ src/main/java/com/ticketpark/ticketpark/rpa/WaitingQueueScheduler.java
@@ -24,7 +24,7 @@ public class WaitingQueueScheduler {
 
         for (String key : waitingKeys) {
             String eventId = key.split(":")[2];
-            queueService.allowUsers(eventId, 50);
+            queueService.allowUsers(eventId, 80);
         }
     }
 }
