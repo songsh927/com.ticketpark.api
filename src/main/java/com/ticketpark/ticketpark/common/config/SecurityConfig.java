9aecd3cd657067fca1221ba7ccce62cdf41459fe
songsh927 <songsh96927@gmail.com>
2025-12-31 01:24:46 +0900

    파일정리 cors 추가

--- src/main/java/com/ticketpark/ticketpark/common/config/SecurityConfig.java
+++ src/main/java/com/ticketpark/ticketpark/common/config/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +28,6 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        // AuthenticationConfiguration 객체로부터 AuthenticationManager를 가져옵니다.
         return authenticationConfiguration.getAuthenticationManager();
     }
 
@@ -31,15 +35,31 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http, JwtCommonFilter jwtCommonFilter) throws Exception{
         http
                 .addFilterBefore(jwtCommonFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/member/myinfo", "/member/update", "/member/logout").authenticated()
                         .requestMatchers("/**").permitAll()
                 );
 
 
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
     @Bean
